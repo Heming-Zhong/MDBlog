3a172f125c9356735e2ed0da07b902abf425b484
learn-by-consciousness <1059397226@qq.com>
2021-06-24 14:04:34 +0800

    新建文件(夹)

---
+++ src/middleground/Loginhandle.java
@@ -0,0 +1,15 @@
+public class Loginhandle{
+
+    public boolean validate(string user, string passwd){
+        return login(user, passwd);
+    }
+
+    public boolean register(string username, string passwd){
+
+
+    }
+
+    private DBmanager manager;
+    private OperationState state;
+};
+
