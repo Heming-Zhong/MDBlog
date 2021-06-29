d3f7d20ce36ca5db7cfb1826df172ad0bf2e5f3d
learn-by-consciousness <1059397226@qq.com>
2021-06-29 18:12:22 +0800

    类定义

--- src/middleground/Loginhandle.java
+++ src/middleground/Loginhandle.java
@@ -1,30 +1,34 @@
-public class Loginhandle{
-    public boolean validate(string user, string passwd){
-        //DBmanager manager = new DBmanager();
-        OperationState state = obj.manager.login(user, passwd);
-        message = state.msg;
-        // switch (state.retState) {
-        //     case OperationState.State.normal:
-        //         message = state.msg;
-        //         break;
-        //     case OperationState.State.warning:
-        
-        //         break;
 
-        //     default:
-        //         break;
-        // }
-        
-        return state.retState == OperationState.State.normal;
+/**登陆控制
+ * 首先实例化一个Loginhandle,
+ * 使用validate(user, passname)来登陆
+ * register(user, passwd)来注册
+ * 使用文件管理类的时候,
+ * 需要通过该类的实例来使用
+ * 
+ */
+import FileManager;
+
+public class Loginhandle{
+    public boolean validate(String user, String passwd){
+        // DBmanager manager = new DBmanager();
+        OperationState state = obj.manager.login(user, passwd);
+        loged = state.retState == OperationState.State.normal;
+        return loged;
     }
 
-    public boolean register(string user, string passwd){
+    public boolean register(String user, String passwd){
         //to do
-
+        // manager = new DBmanager();
+        // OperationState state = manager.register(user, passwd, permission);
+        // loged = state.retState == OperationState.State.normal;
+        // return loged;
+        return false;
     }
 
     
     private FileManager obj;
     public String message;
+    private boolean loged;
 };
 
